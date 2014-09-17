29ff245cc3e8c96534854a0f9b4f59db79f7efd4
garrett <g.kocher@gmail.com>
2014-09-16 18:31:16 -0700

    update welcome views

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -12,7 +12,7 @@ describe 'Index Page' do
       expect(page).to have_selector 'h1', text: 'Welcome to FDA Import Alerts'
     end
     it 'requests users for email' do
-      expect(page).to have_selector 'h3', text: 'Would email updates to one of the FDA alerts be useful to you?'
+      expect(page).to have_selector 'h3', text: 'Would email updates to the FDA alerts be useful to you?'
     end
     it 'has a link to the FDA website' do
       expect(page).to have_link "", "http://www.fda.gov/ForIndustry/ImportProgram/ImportAlerts/"
