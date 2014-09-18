ed5210fa1731d56267dda95adbb1c62c6e8994ee
garrett <g.kocher@gmail.com>
2014-09-18 14:00:37 -0700

    update pesticides views

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,5 +1,6 @@
 class AlertsController < ApplicationController
   def pesticides
-    @products = Product.order('date(date_published) DESC').limit(10)
+    @products = Product.order('date(date_published)').includes(:company, :pesticides)
+    @product_months = @products.group_by {|p| p.date_published.beginning_of_month }
   end
 end
