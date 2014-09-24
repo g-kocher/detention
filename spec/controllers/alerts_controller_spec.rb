68cf7d38dd587916dc8a16a9d5c569d8b58da9c3
garrett <g.kocher@gmail.com>
2014-09-24 12:53:19 -0700

    update alerts and welcome controller spec

--- spec/controllers/alerts_controller_spec.rb
+++ spec/controllers/alerts_controller_spec.rb
@@ -6,9 +6,5 @@ RSpec.describe AlertsController, :type => :controller do
     it "returns http success" do
       expect(response).to be_success
     end
-    it 'returns @products' do
-      expect(@products).to_not be_nil
-    end
   end
-
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
 
+describe "GET index" do
+    before(:each) { get :index }
+    it "returns http success" do
+      expect(response).to be_success
+    end
+  end
+
 end
