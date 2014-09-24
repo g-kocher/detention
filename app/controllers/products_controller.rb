9a2c6fc4ec76a906037442dd011e146826b67dfc
garrett <g.kocher@gmail.com>
2014-09-24 12:53:02 -0700

    begin products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,6 @@
+class ProductsController < ApplicationController
+  def show
+    @product = Product.includes(:company, :pesticides).find(params[:id])
+    @related_products = Product.where(company_id: @product.company_id).where.not(id: @product.id)
+  end
+end
---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ProductsController, :type => :controller do
+
+  describe "GET show" do
+    before(:each) { get :show, id: 1 }
+    it "returns http success" do
+      expect(response).to be_success
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+  end
+end
