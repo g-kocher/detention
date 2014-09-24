fb9384edfd282e0074a8f94b76677c05d4dfeca3
garrett <g.kocher@gmail.com>
2014-09-24 13:21:24 -0700

    add format_address helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def format_product_name name
     name.gsub(/([0-9]{2}\W[A-Z,-]\W[A-Z,-]\W[A-Z,-]\W[0-9,-]{2}\W\W)/, "")
   end
+  def format_address address
+    address.gsub(",", '<br />').html_safe
+  end
 end
