60d59095a7144da019e4ed5f5d14eca84ab0984e
garrett <g.kocher@gmail.com>
2014-09-24 14:20:53 -0700

    update views

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,6 +1,5 @@
 class AlertsController < ApplicationController
   def pesticides
-    @companies = Company.includes(:products).order('products.date_published DESC').limit(200)
-
+    @companies = Company.includes(:products).order('products.date_published DESC').where("products.date_published BETWEEN '#{Date.today - 1.month}' AND '#{Date.today}'")
   end
 end
