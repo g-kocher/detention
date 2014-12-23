96a474af2fd19c7426ed7489f895cfd19fbae625
garrett <g.kocher@gmail.com>
2014-12-22 16:52:59 -0800

    update import of new data

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,13 +2,11 @@ class Import < ActiveRecord::Base
   validates :items_created, presence: true
 
   def refresh data=client.fetch
-    initial_product_count = Product.count
     site_data = data
-    create_records_from site_data
-    items_created = Product.count - initial_product_count
+    new_products = create_records_from site_data
     save
-    if items_created > 0
-      send_mail items_created
+    if new_products.count > 0
+      send_mail new_products.count
     end
   end
 
@@ -22,10 +20,14 @@ class Import < ActiveRecord::Base
   end
 
   def create_records_from(data)
+    new_products = []
     data.each do |comp|
       company = Company.where(name: comp[:name], address: comp[:address]).first_or_create
       comp[:products].each do |prod|
-        product = Product.where(company_id: company.id, name: prod[:name], date_published: prod[:date]).first_or_create
+        product = Product.where(company_id: company.id, name: prod[:name]).first_or_create do |product|
+          product.date_published = prod[:date]
+          new_products << product
+        end
         unless prod[:pesticides].nil?
           prod[:pesticides].each do |pest|
             pesticide = Pesticide.where(name: pest).first_or_create
@@ -34,5 +36,6 @@ class Import < ActiveRecord::Base
         end
       end
     end
+    return new_products
   end
 end
--- lib/tasks/refresh_data.rake
+++ lib/tasks/refresh_data.rake
@@ -2,6 +2,7 @@ desc "This task runs the FDA client to refresh the site data"
 task refresh_data: :environment do
   puts 'Updating data...'
   import = Import.new
+  time = Time.now
   import.refresh
-  puts 'done'
+  puts "done in: #{Time.now - time} seconds!"
 end
